a4f388f2612552295ec75564a75cc9365e1328eb
Sam Pikesley <sam.pikesley@demandlogic.co.uk>
2026-03-28 22:43:15 +0000

    Test vert. start position

---
+++ tests/lemmings/test_digger.py
@@ -0,0 +1,17 @@
+from lib.lemmings.digger import Digger
+from tests.helpers import test_defaults
+
+params = dict(test_defaults, name="digger")
+
+
+def test_digger():
+    """Test."""
+    d = Digger(params)
+    assert d.name == "digger"
+
+def test_start_position():
+    """Test."""
+    d = Digger(params)
+
+    assert d.x == 0
+    assert d.y == -172 # aha this is fucked
