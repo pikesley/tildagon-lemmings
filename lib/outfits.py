ca407206c17ec9ab196e75ce3b3de5a0005ff550
Sam Pikesley <sam.pikesley@demandlogic.co.uk>
2026-03-28 22:43:02 +0000

    Unify outfit

--- lib/outfits.py
+++ lib/outfits.py
@@ -1,5 +1,3 @@
-from random import choice
-
 try:
     from ..common.colour_tools import rgb_from_hue
 except ImportError:
@@ -14,34 +12,7 @@ except FileNotFoundError:
     conf = json.loads(Path("conf.json").read_text())
 
 
-class Outfit:
-    """Base class."""
-
-    def __getitem__(self, key):
-        """`foo[key]`."""
-        return self.data[key]
-
-    def get(self, key, default):
-        """`foo.get(key)`."""
-        if key in self.data:
-            return self.data[key]
-        return default
-
-
-class RandomOutfit(Outfit):
-    """Lemming clothes."""
-
-    def __init__(self, _):
-        """Construct."""
-        self.colours = list(conf["colours"].values())
-        self.keys = ["hr", "cl"]
-
-        self.data = dict(conf["default-colours"])
-        for key in self.keys:
-            self.data[key] = choice(self.colours)
-
-
-class RotatingOutfit(Outfit):
+class RotatingOutfit:
     """Lemming clothes."""
 
     def __init__(self, hue):
@@ -54,14 +25,12 @@ class RotatingOutfit(Outfit):
             "dt": rgb_from_hue(hue + 2 / 3),
         }
 
+    def __getitem__(self, key):
+        """`foo[key]`."""
+        return self.data[key]
 
-class TriadicOutfit(Outfit):
-    """Lemming clothes."""
-
-    def __init__(self, hue):
-        """Construct."""
-        self.data = {
-            "sk": rgb_from_hue(hue),
-            "hr": rgb_from_hue(hue + 1 / 3),
-            "cl": rgb_from_hue(hue + 2 / 3),
-        }
+    def get(self, key, default):
+        """`foo.get(key)`."""
+        if key in self.data:
+            return self.data[key]
+        return default
